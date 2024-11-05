af9d982a1cfd1a2d3df7da4f30b2dbbe7e5f56a6
SouthKioto <bolisegabartek6@gmail.com>
2024-11-05 19:33:44 +0100

    stworzenie projektu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 function App() {
+  let [title, setTitle] = useState('');
+  let [type, setType] = useState('');
+
+  const submitHandler = e => {
+    e.preventDefault();
+    console.log('Tytuł: ' + title + ' Rodzaj: ' + type);
+  };
+
+  const titleInputValueChange = e => {
+    setTitle(e.target.value);
+  };
+
+  const selectInputValueChange = e => {
+    setType(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form>
+        <Form.Group className='mb-3'>
+          <Form.Label>Tytuł filmu</Form.Label>
+          <Form.Control type='text' value={title} onChange={titleInputValueChange} />
+        </Form.Group>
+        <Form.Group className='mb-3'>
+          <Form.Label>Rodzaj</Form.Label>
+          <Form.Select name='formSelect' value={type} onChange={selectInputValueChange}>
+            <option value={1}>Komedia</option>
+            <option value={2}>Obyczajowy</option>
+            <option value={3}>Sensacyjny</option>
+            <option value={4}>Horror</option>
+          </Form.Select>
+        </Form.Group>
+        <Button variant='primary' type='submit' onClick={submitHandler}>
+          Dodaj
+        </Button>
+      </Form>
     </div>
   );
 }
